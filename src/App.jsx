b61b3be6fe9f77d262289857dbaf7773f51cd2bc
htoo-myat <htoomyatyeeaung20@gmail.com>
2026-06-22 14:33:20 +0900

    database up

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-
-import './App.css'
+import { useEffect } from 'react'
+import { supabase } from '../supabase.js'
 
 function App() {
+  useEffect(() => {
+    async function testConnection() {
+      const { data, error } = await supabase
+        .from('stores') // table name ကိုယ့် table နာမည်နဲ့ပြောင်း
+        .select('*')
+        .limit(1)
 
-  return (
-    <>
-      <h1>asiudasiud</h1>
-      <div>asdsa</div>
-    </>
-  )
+      if (error) {
+        console.error('Connection Error:', error)
+      } else {
+        console.log('Connected Successfully!')
+        console.log(data)
+      }
+    }
+
+    testConnection()
+  }, [])
+
+  return <h1>Testing Supabase</h1>
 }
 
 export default App
